2e23912ab3009d3f2174b5c2562d0eba9e8cd861
Erica <erica.wang.moyang@gmail.com>
2019-04-14 23:40:28 -0400

    better work

--- api/server.js
+++ api/server.js
@@ -16,8 +16,17 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 app.use('/engisp', resultRoutes);
+
+app.use(express.static(__dirname + '/dist/engisp'));
+
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/engisp/index.html'));
+});
+
 const port = process.env.PORT || 4000;
 
 const server = app.listen(port, function(){
  console.log('Listening on port ' + port);
 });
+
