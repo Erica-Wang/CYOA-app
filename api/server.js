080f9d60c3dc4b86888f3e1e51a01183cf13e241
Erica <erica.wang.moyang@gmail.com>
2019-04-17 21:27:36 -0400

    initial commit

--- api/server.js
+++ api/server.js
@@ -9,6 +9,8 @@ const resultRoutes = require('./result.routes');
 
 require('dotenv').config();
 
+console.log(process.env.MONGODB_URI);
+
 mongoose.Promise = global.Promise;
 
 mongoose.connect(process.env.MONGODB_URI);
@@ -25,12 +27,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded())
 app.use(cors());
-app.use('/engisp', resultRoutes);
-
-app.use(express.static(__dirname + '/../dist/engisp'));
-app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+'/../dist/engisp/index.html'));
-});
+app.use('/', resultRoutes);
 
 const port = process.env.PORT || 4000;
 
